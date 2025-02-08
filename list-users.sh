41fa386cbbbdcc23ce29147cb6fc2f6ae61094f3
sawansa1993 <sawantsagar006@gmail.com>
2025-02-08 12:28:02 +0530

    Create list-users.sh

---
+++ list-users.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###################################
+# Author: Sagar Sawant
+# Script is used to fetch users 
+# details
+# Date:08-02-2025
+###################################
+
+#curl -L \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "Authorization: Bearer <YOUR-TOKEN>" \
+#  -H "X-GitHub-Api-Version: 2022-11-28" \
+#  https://api.github.com/repos/OWNER/REPO/pulls
+
+#API URL
+API_URL = "https://api.github.com"
+
+#Information need to export 
+USERNAME = $username
+TOKEN = $token
+
+#Arguments
+REPO_OWNER = $1
+REPO_NAME = $2
+
+function 
+
+   
+
+function 
